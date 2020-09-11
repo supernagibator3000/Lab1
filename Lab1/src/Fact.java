144f117208d5276cfc2102498d5e99b2782d1c32
supernagibator3000 <foxsfoxs765@gmail.com>
2020-09-11 10:09:12 +0300

    Lab1 1st try

--- Lab1/src/Fact.java
+++ Lab1/src/Fact.java
@@ -5,9 +5,10 @@ public class Fact
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        int num, res = 0;
+        int num, res = 1;
         num = sc.nextInt();
-        for (int i = 1; i < num; i++)
-            res *= num;
+        for (int i = 1; i <= num; i++)
+            res *= i;
+        System.out.print(num + "! = " + res);
     }
 }
