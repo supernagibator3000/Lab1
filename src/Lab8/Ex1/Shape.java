77e01e0d008394214a99a9c8c7337a243268736c
supernagibator3000 <foxsfoxs765@gmail.com>
2020-10-01 09:38:32 +0300

    <=Lab8

--- src/Lab8/Ex1/Shape.java
+++
@@ -1,8 +0,0 @@
-package Lab8.Ex1;
-
-import java.awt.*;
-
-public abstract class Shape {
-    Color color;
-    int x, y;
-}
--- src/Lab8/Ex1/ShapeCreation.java
+++ src/Lab8/Ex1/ShapeCreation.java
@@ -1,17 +1,37 @@
 package Lab8.Ex1;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
 
 public class ShapeCreation extends JFrame {
     public ShapeCreation(){
         setTitle("Создание фигур");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 500);
 
+        Circle circle;
+        Square square;
 
+        int width = 500;
+        int height = 400;
+        setSize(width + 15, height + 40);
 
-        setResizable(true);
-        setVisible(true);
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 4; j++){
+                switch((int)(Math.random()*2)){
+                    case 0:
+                        circle = new Circle(i*width/5,j*height/4, width/5 - 4,height/4 - 4);
+                        add(circle);
+                        break;
+                    case 1:
+                        square = new Square(i*width/5,j*height/4, width/5 - 4,height/4 - 4);
+                        add(square);
+                        break;
+                }
+                setVisible(true);
+            }
+        }
+        setResizable(false);
     }
 
     public static void main(String[] args) {
