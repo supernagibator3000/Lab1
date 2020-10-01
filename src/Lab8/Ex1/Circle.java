dd74932ba9823718e766642cf958d1c332162c0b
supernagibator3000 <foxsfoxs765@gmail.com>
2020-10-01 09:43:44 +0300

    <=Lab8

--- src/Lab8/Ex1/Circle.java
+++ src/Lab8/Ex1/Circle.java
@@ -1,5 +1,23 @@
 package Lab8.Ex1;
 
-public class Circle extends Shape{
+import javax.swing.*;
+import java.awt.*;
 
+public class Circle extends JComponent{
+    private int x, y, width, height;
+
+    public Circle(int x, int y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void paint(Graphics g){
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D)g;
+        Color color = new Color((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256));
+        g2d.setPaint(color);
+        g2d.fillOval(x, y, width, height);
+    }
 }
