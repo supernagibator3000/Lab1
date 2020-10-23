24308d6845c663dd67b786797cb5ad695e9c629d
supernagibator3000 <foxsfoxs765@gmail.com>
2020-10-23 14:13:08 +0300

    +Lab13+~Lab14

---
+++ src/Lab12/GUI.java
@@ -0,0 +1,75 @@
+package Lab12;
+
+import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class GUI extends JFrame {
+    String[] planets = {"Не выбрано", "Меркурии", "Венере", "Земле", "Марсе", "Юпитере", "Сатурне", "Уране", "Нептуне"};
+
+    public GUI(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+        setTitle("Поверхностная гравитация");
+        setSize(300, 120);
+        setResizable(false);
+
+        JLabel text = new JLabel("Поверхностная гравитация на ");
+        JComboBox planetSelector = new JComboBox(planets);
+        JLabel gravityLabel = new JLabel();
+
+        planetSelector.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                String planet = (String) planetSelector.getSelectedItem();
+                switch(planet){
+                    default:
+                        gravityLabel.setVisible(false);
+                        break;
+                    case "Меркурии":
+                        gravityLabel.setText("g = " + Planet.MERCURY.getGravity() + " м/с^2");
+                        gravityLabel.setVisible(true);
+                        break;
+                    case "Венере":
+                        gravityLabel.setText("g = " + Planet.VENUS.getGravity() + " м/с^2");
+                        gravityLabel.setVisible(true);
+                        break;
+                    case "Земле":
+                        gravityLabel.setText("g = " + Planet.EARTH.getGravity() + " м/с^2");
+                        gravityLabel.setVisible(true);
+                        break;
+                    case "Марсе":
+                        gravityLabel.setText("g = " + Planet.MARS.getGravity() + " м/с^2");
+                        gravityLabel.setVisible(true);
+                        break;
+                    case "Юпитере":
+                        gravityLabel.setText("g = " + Planet.JUPITER.getGravity() + " м/с^2");
+                        gravityLabel.setVisible(true);
+                        break;
+                    case "Сатурне":
+                        gravityLabel.setText("g = " + Planet.SATURN.getGravity() + " м/с^2");
+                        gravityLabel.setVisible(true);
+                        break;
+                    case "Уране":
+                        gravityLabel.setText("g = " + Planet.URANUS.getGravity() + " м/с^2");
+                        gravityLabel.setVisible(true);
+                        break;
+                    case "Нептуне":
+                        gravityLabel.setText("g = " + Planet.NEPTUNE.getGravity() + " м/с^2");
+                        gravityLabel.setVisible(true);
+                        break;
+                }
+            }
+        });
+
+        text.setBounds(5, 10, 180, 20);
+        planetSelector.setBounds(185, 10, 95, 20);
+        gravityLabel.setBounds(5, 40, 200, 20);
+
+        add(text);
+        add(planetSelector);
+        add(gravityLabel);
+
+        setVisible(true);
+    }
+}
